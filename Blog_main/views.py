bcb013e2047fd225134d116e5d7d24099640b448
sai ashish pinnamsetty <saiashish611999@gmail.com>
2025-10-21 15:08:39 +0530

    diplayed categories and posts

--- Blog_main/views.py
+++ Blog_main/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 def home(request):
-  return render(request, "home.html")
+  categories = Category.objects.all()
+  featured_posts = Blog.objects.filter(is_featured = True)
+  print(featured_posts)
+  context = {
+    'categories':categories,
+    'featured_posts':featured_posts
+  }
+  return render(request, "home.html", context)
--- blogs/models.py
+++ blogs/models.py
@@ -24,7 +24,7 @@ class Blog(models.Model):
   slug = models.SlugField(max_length = 350, unique = True, blank = True)
   category = models.ForeignKey(Category, on_delete = models.CASCADE)
   author = models.ForeignKey(User, on_delete = models.CASCADE)
-  featured_image = models.ImageField(upload_to = 'uploads/%Y/%m/%d')
+  featured_image = models.ImageField(upload_to = 'uploads/%Y/%m/%d', blank = True)
   short_description = models.TextField(max_length = 500)
   blog_body = models.TextField(max_length = 2000)
   status = models.CharField(max_length= 20, choices=STATUS_CHOICES, default="draft")
